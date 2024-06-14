d545bebc6560392993f085230a88677f48abb48f
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-06-14 15:36:41 +0900

    add mockito

--- apps/api-mock/src/index.ts
+++ apps/api-mock/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/2/users/me', (c) => {
-  return c.json({data: {id: '123', name: 'abc', username: 'xyz'}})
+  return c.json({data: {id: '999', name: '予定表～①💖ﾊﾝｶｸだ', username: 'yoteihyo'}})
 })
 
 export default app
--- apps/sample/src/main.rs
+++ apps/sample/src/main.rs
@@ -1,13 +1,20 @@
-use twapi_v2::{api::get_2_users_me, oauth10a::OAuthAuthentication};
+use mockito::{Mock, Server, ServerGuard};
+use serde_json::json;
+use twapi_v2::{api::{get_2_users_me, TwapiOptions}, oauth10a::OAuthAuthentication};
 
-async fn execute_x() -> anyhow::Result<()> {
+async fn execute_x(twapi_options: Option<TwapiOptions>) -> anyhow::Result<()> {
     let auth = OAuthAuthentication::new(
         std::env::var("CONSUMER_KEY").unwrap_or_default(),
         std::env::var("CONSUMER_SECRET").unwrap_or_default(),
         std::env::var("ACCESS_KEY").unwrap_or_default(),
         std::env::var("ACCESS_SECRET").unwrap_or_default(),
     );
-    match get_2_users_me::Api::new().execute(&auth).await {
+    let mut api = get_2_users_me::Api::new();
+    if let Some(twapi_options) = twapi_options {
+        api = api.twapi_options(twapi_options);
+    }
+    match api.execute(&auth).await 
+    {
         Ok((response, _rate_limit)) => {
             println!("{:?}", response);
             Ok(())
@@ -16,9 +23,42 @@ async fn execute_x() -> anyhow::Result<()> {
     }
 }
 
+async fn make_mock() -> (ServerGuard, Mock) {
+    let mut server = Server::new_async().await;
+    let json = json!({
+        "data": {
+            "id": "123",
+            "name": "モック",
+            "username": "mock",
+        }
+    });
+    let mock = server
+        .mock("GET", "/2/users/me")
+        .match_query(mockito::Matcher::Any)
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(json.to_string())
+        .create_async()
+        .await;
+    (server, mock)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let _ = execute_x().await?;
+    // モック無し
+    let _ = execute_x(None).await?;
+
+    // モックあり
+    let (server, mock) = make_mock().await;
+    let twapi_options = Some(TwapiOptions {
+        prefix_url: Some(server.url()),
+        ..Default::default()
+    });
+    let _ = execute_x(twapi_options).await?;
+    mock.assert();
+
+    // 外部モック
+    // TWAPI_V2_TWITTER_API_PREFIX_API=http://localhost:3000 cargo run
 
     Ok(())
 }
